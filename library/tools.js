da1039df63913e890687a890c888324dc0d97ae0
mrbungle64 <mrb1232@posteo.de>
2020-02-16 10:15:08 +0100

    Remove unused xmlDocumentElement2Json function

--- library/tools.js
+++ library/tools.js
@@ -32,7 +32,6 @@ function getEventNameForCommandString(str) {
             return 'ChargeState';
         case "battery":
         case 'batteryinfo':
-        case 'battery':
             return 'BatteryInfo';
         case 'lifespan':
             return 'LifeSpan';
@@ -50,22 +49,6 @@ function getEventNameForCommandString(str) {
     }
 }
 
-function xmlDocumentElement2Json(element) {
-    let json = {};
-    json[element.nodeName] = {};
-    for (let a = 0; a < element.attributes.length; a++) {
-        json[element.nodeName][element.attributes[a].name] = element.attributes[a].value;
-    }
-    for (let c = 0; c < element.childNodes.length; c++) {
-        let child = element.childNodes[c];
-        json[element.nodeName][child.nodeName] = {};
-        for (a = 0; a < child.attributes.length; a++) {
-            json[element.nodeName][child.nodeName][child.attributes[a].name] = child.attributes[a].value;
-        }
-    }
-    return json;
-}
-
 envLog = function () {
     if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "dev") {
         console.log.apply(this, arguments);
@@ -75,5 +58,4 @@ envLog = function () {
 module.exports.isObject = isObject;
 module.exports.isValidJsonString = isValidJsonString;
 module.exports.getEventNameForCommandString = getEventNameForCommandString;
-module.exports.xmlDocumentElement2Json = xmlDocumentElement2Json;
 module.exports.envLog = envLog;
