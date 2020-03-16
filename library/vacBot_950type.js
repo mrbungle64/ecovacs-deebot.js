a4ce8c5cd950b48a46c9126ea3e8ca2cf428e43b
boriswerner <bwerner@uni-osnabrueck.de>
2020-03-16 18:31:29 +0100

    fix chargeState when canceling return to dock

--- library/vacBot_950type.js
+++ library/vacBot_950type.js
@@ -227,6 +227,10 @@ class VacBot_950type {
         if(dictionary.CLEAN_MODE_FROM_ECOVACS[event['resultData']['state']] == 'returning') { //set charge state on returning to dock
           this.charge_status = dictionary.CLEAN_MODE_FROM_ECOVACS[event['resultData']['state']];
           tools.envLog("[VacBot] *** charge_status = %s", this.charge_status);
+        } else if(dictionary.CLEAN_MODE_FROM_ECOVACS[event['resultData']['state']] == 'idle') {
+          //when clean state = idle the bot can be charging on the dock or the return to dock has been canceled
+          //if this is not run, the status when canceling the return stays on 'returning'
+          this.run('GetChargeState');
         }
       }
     } else {
