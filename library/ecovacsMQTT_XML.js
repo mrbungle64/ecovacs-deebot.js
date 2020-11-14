b68082bfc6afe839f024b6fc6cad2c8a962dd5b7
mrbungle64 <mrb1232@posteo.de>
2020-11-14 17:16:12 +0100

    Derive ecovacsMQTT_XML class from ecovacsMQTT

--- library/ecovacsMQTT_XML.js
+++ library/ecovacsMQTT_XML.js
@@ -9,52 +9,7 @@ const dictionary = require('./ecovacsConstants_non950type');
 class EcovacsMQTT_XML extends EcovacsMQTT {
     constructor(bot, user, hostname, resource, secret, continent, country, vacuum, server_address, server_port = 8883) {
         super(bot, user, hostname, resource, secret, continent, country, vacuum, server_address, server_port);
-
-        this.mqtt = require('mqtt');
-        this.client = null;
-
-        this.customdomain = hostname.split(".")[0]; // MQTT is using domain without tld extension
-        this.username = user + '@' + this.customdomain;
-        this.clientId = this.username + '/' + resource;
-
-        let options = {
-            clientId: this.clientId,
-            username: this.username,
-            password: this.secret,
-            rejectUnauthorized: false
-        };
-
-        let url = 'mqtts://' + this.server_address + ':' + this.server_port;
-        this.client = this.mqtt.connect(url, options);
-        tools.envLog("[EcovacsMQTT] Connecting as %s to %s", this.username, url);
-
-        let vacuum_did = this.vacuum['did'];
-        let vacuum_class = this.vacuum['class'];
-        let vacuum_resource = this.vacuum['resource'];
-        let ecovacsMQTT = this;
-
-        this.client.on('connect', function () {
-            tools.envLog('[EcovacsMQTT] client connected');
-            this.subscribe('iot/atr/+/' + vacuum_did + '/' + vacuum_class + '/' + vacuum_resource + '/+', (error, granted) => {
-                if (!error) {
-                    ecovacsMQTT.emit('ready', 'Client connected. Subscribe successful');
-                } else {
-                    tools.envLog('[EcovacsMQTT] subscribe err: %s', error.toString());
-                }
-            });
-        });
-
-        this.client.on('message', (topic, message) => {
-            tools.envLog('[EcovacsMQTT] message.toString(): ', message.toString());
-            this._handle_message(topic, message.toString(), "incoming");
-        });
-
-        this.client.on('error', (error) => {
-            ecovacsMQTT.emit('error', error.toString());
-        });
     }
-
-
 
     send_command(action, recipient) {
         let c = this._wrap_command(action, recipient);
@@ -390,20 +345,6 @@ class EcovacsMQTT_XML extends EcovacsMQTT {
             default:
                 tools.envLog("[EcovacsMQTT] Unknown command received: %s", command);
                 break;
-        }
-    }
-
-    send_ping(to) {
-        return true;
-    }
-
-    //end session
-    disconnect() {
-        try {
-            this.client.end();
-            tools.envLog("[EcovacsMQTT] Closed MQTT Client");
-        } catch (e) {
-            tools.envLog("[EcovacsMQTT] Error closing MQTT Client:  %s", e.toString());
         }
     }
 }
