4d738e70afe47739d1a8a59930680b576dc6e7df
mrbungle64 <mrb1232@posteo.de>
2021-04-18 18:25:14 +0200

    Some minor changes to test.js file

--- test/test.js
+++ test/test.js
@@ -7,55 +7,55 @@ describe('API', function () {
   describe('error checking', function () {
     it('should throw an error when no arguments are provided', function () {
       assert.throws(() => {
-        let api = new ecovacsDeebot.EcoVacsAPI();
+        const api = new ecovacsDeebot.EcoVacsAPI();
       });
     });
 
     it('should throw an error when no country or continent is provided', function () {
       assert.throws(() => {
-        let api = new ecovacsDeebot.EcoVacsAPI("abcdefghijklmnopqrestuvwyz");
+        const api = new ecovacsDeebot.EcoVacsAPI("abcdefghijklmnopqrestuvwyz");
       });
     });
 
     it('should throw an error when no continent is provided', function () {
       assert.throws(() => {
-        let api = new ecovacsDeebot.EcoVacsAPI("abcdefghijklmnopqrestuvwyz", "nl");
+        const api = new ecovacsDeebot.EcoVacsAPI("abcdefghijklmnopqrestuvwyz", "nl");
       });
     });
 
     it('should not throw an error when all arguments are provided to the constructor', function () {
       assert.doesNotThrow(() => {
-        let api = new ecovacsDeebot.EcoVacsAPI("abcdefghijklmnopqrestuvwyz", "nl", "eu");
+        const api = new ecovacsDeebot.EcoVacsAPI("abcdefghijklmnopqrestuvwyz", "nl", "eu");
       });
     });
   });
 
   describe('storing variables', function () {
     it('should store the country and device id parameter in a meta variable', function () {
-      let device_id = "abcdefghijklmnopqrestuvwyz";
-      let country = "nl";
-      let api = new ecovacsDeebot.EcoVacsAPI(device_id, country, "eu");
-      assert.equal(api.meta.deviceId, device_id);
-      assert.equal(api.meta.country, country);
+      const device_id = "abcdefghijklmnopqrestuvwyz";
+      const country = "nl";
+      const api = new ecovacsDeebot.EcoVacsAPI(device_id, country, "eu");
+      assert.strictEqual(api.meta.deviceId, device_id);
+      assert.strictEqual(api.meta.country, country);
     });
 
     it('should store the first 8 characters of the device id as the resource id', function () {
-      let api = new ecovacsDeebot.EcoVacsAPI("abcdefghijklmnopqrestuvwyz", "nl", "eu");
-      assert.equal(api.resource, "abcdefgh");
+      const api = new ecovacsDeebot.EcoVacsAPI("abcdefghijklmnopqrestuvwyz", "nl", "eu");
+      assert.strictEqual(api.resource, "abcdefgh");
     });
 
     it('should store the country provided', function () {
-      let country = "nl";
-      let api = new ecovacsDeebot.EcoVacsAPI("abcdefghijklmnopqrestuvwyz", country, "eu");
+      const country = "nl";
+      const api = new ecovacsDeebot.EcoVacsAPI("abcdefghijklmnopqrestuvwyz", country, "eu");
       assert.ok(api.country);
-      assert.equal(api.country, country);
+      assert.strictEqual(api.country, country);
     });
 
     it('should store the continent provided', function () {
-      let continent = "eu";
-      let api = new ecovacsDeebot.EcoVacsAPI("abcdefghijklmnopqrestuvwyz", "nl", continent);
+      const continent = "eu";
+      const api = new ecovacsDeebot.EcoVacsAPI("abcdefghijklmnopqrestuvwyz", "nl", continent);
       assert.ok(api.continent);
-      assert.equal(api.continent, continent);
+      assert.strictEqual(api.continent, continent);
     });
   });
 
@@ -69,7 +69,7 @@ describe('API', function () {
 
     it('should be a valid key file', function () {
       assert.doesNotThrow(() => {
-        let encrypted = crypto.publicEncrypt({
+        const encrypted = crypto.publicEncrypt({
           key: fs.readFileSync("key.pem", "utf8"),
           padding: crypto.constants.RSA_PKCS1_PADDING
         }, new Buffer("unencrypted")).toString('base64');
@@ -81,21 +81,21 @@ describe('API', function () {
 describe('API tools', function () {
   describe('#isObject', function () {
     it('should check if a value is truly an object', function () {
-      assert.equal(tools.isObject(null), false);
-      assert.equal(tools.isObject("test"), false);
-      assert.equal(tools.isObject(String("test")), false);
-      assert.equal(tools.isObject(100), false);
-      assert.equal(tools.isObject(100.5), false);
-      assert.equal(tools.isObject(true), false);
-      assert.equal(tools.isObject(undefined), false);
-      assert.equal(tools.isObject(Symbol()), false);
-      assert.equal(tools.isObject({}), true);
-      assert.equal(tools.isObject({key: "value"}), true);
-      assert.equal(tools.isObject(JSON.parse('{"key": "value"}')), true);
-      assert.equal(tools.isObject(() => {
+      assert.strictEqual(tools.isObject(null), false);
+      assert.strictEqual(tools.isObject("test"), false);
+      assert.strictEqual(tools.isObject(String("test")), false);
+      assert.strictEqual(tools.isObject(100), false);
+      assert.strictEqual(tools.isObject(100.5), false);
+      assert.strictEqual(tools.isObject(true), false);
+      assert.strictEqual(tools.isObject(undefined), false);
+      assert.strictEqual(tools.isObject(Symbol()), false);
+      assert.strictEqual(tools.isObject({}), true);
+      assert.strictEqual(tools.isObject({key: "value"}), true);
+      assert.strictEqual(tools.isObject(JSON.parse('{"key": "value"}')), true);
+      assert.strictEqual(tools.isObject(() => {
       }), true);
-      assert.equal(tools.isObject(new Object()), true);
-      assert.equal(tools.isObject(new Date()), true);
+      assert.strictEqual(tools.isObject(new Object()), true);
+      assert.strictEqual(tools.isObject(new Date()), true);
     });
   });
 
@@ -105,15 +105,14 @@ describe('API tools', function () {
     });
 
     it('should replace key identifiers with provided values', function () {
-      assert.equal("{first} {second}".format({first: "Hello", second: "world"}), "Hello world");
-      assert.equal("{first} world".format({first: "Hello"}), "Hello world");
+      assert.strictEqual("{first} {second}".format({first: "Hello", second: "world"}), "Hello world");
+      assert.strictEqual("{first} world".format({first: "Hello"}), "Hello world");
     });
 
     it('should not replace key identifiers when not provided as values', function () {
-      assert.equal("{first} {second}".format({foo: "Hello", bar: "world"}), "{first} {second}");
-      assert.equal("{first} world".format({foo: "Hello", bar: "world"}), "{first} world");
-      assert.equal("{first} {second}".format({}), "{first} {second}");
-      assert.equal("{first} {second}".format(), "{first} {second}");
+      assert.strictEqual("{first} {second}".format({foo: "Hello", bar: "world"}), "{first} {second}");
+      assert.strictEqual("{first} world".format({foo: "Hello", bar: "world"}), "{first} world");
+      assert.strictEqual("{first} {second}".format({}), "{first} {second}");
     });
   });
 
@@ -121,17 +120,5 @@ describe('API tools', function () {
     it('should export a countries object', function () {
       assert.ok(ecovacsDeebot.countries);
     });
-
-    it('should export the continent for 252 country codes', function () {
-      let iter = 0;
-      for (let key in ecovacsDeebot.countries) {
-        if (ecovacsDeebot.countries.hasOwnProperty(key)) {
-          if (!!ecovacsDeebot.countries[key].continent) {
-            iter++;
-          }
-        }
-      }
-      assert.equal(iter, 252);
-    })
   });
 });
